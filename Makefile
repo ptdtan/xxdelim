d94bce10f9317c414ce72a5805aa55f716b7203f
Duy Tan Phan Thai <tan@Duys-MacBook-Pro.local>
2021-08-31 17:33:15 +0700

    change main to unittest

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CXXFLAGS += -Wall -msse4.2 -std=c++11 
 all:
-	g++ ${CXXFLAGS} -c main.cpp
+	g++ ${CXXFLAGS} -c unittest.cpp
 	g++ ${CXXFLAGS} -c block_info/genblock.cpp
-	g++ ${CXXFLAGS} -o xxdelim main.o genblock.o 
+	g++ ${CXXFLAGS} -o unittest unittest.o genblock.o 
--- unittest.cpp
+++ unittest.cpp
@@ -2,17 +2,21 @@
 #include <vector>
 #include <iostream>
 #include <iterator>
+#include <assert.h>
 #include "sse/sse_convert.hpp"
 
 using namespace sse;
 
 int main(void) {
   std::string data("2,3333333,34,56,789,0,0,10,0,1000");
+  std::vector<uint32_t> truth{2, 3333333, 34, 56, 789, 0, 0, 10, 0, 1000};
   std::vector<uint32_t> output;
   sse_parse_unsigned(data.c_str(), ',', data.size(), std::back_inserter(output));
   for (int i = 0 ; i < output.size(); ++i) {
     std::cout << output[i] << " ";
+    assert("\033[1;33mFAILED\033[0m" && truth[i] == output[i]);
   }
   std::cout << std::endl;
+  std::cout << "\033[1;32mPASSED\033[0m\n";
   return 0;
 }
